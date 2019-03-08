e38773b99dda11809039ea85d5481e861ae9f184
Amir Hossain <mr.amir.1971@gmail.com>
2019-03-09 02:12:09 +0600

    done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,23 @@
-
 public class Main {
 
 	public static void main(String[] args) {
-
+		int[] a= {85,32,45,87,8,1,369,45,1};
+		for(int i=1;i<a.length;i++) {
+			int j=i;
+			while(j>0) {
+				if(a[j]<a[j-1]) {
+					int temp=a[j-1];
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+				j--;
+			}
+		}
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
 	}
 
 }
